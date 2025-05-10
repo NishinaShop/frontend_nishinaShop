d8becf69e4029c3d1a0de7ec27b946b17ccd7b28
Emanuel Morales <eemoralesb@outlook.com>
2025-05-10 06:05:07 -0600

    consolas log verificacion

--- src/views/validacionView.vue
+++ src/views/validacionView.vue
@@ -71,7 +71,7 @@ export default{
     this.venta.cliente = user_data._id
     if(this.$route.params.direccion){
     this.direccion = this.$route.params.direccion
-    console.log(this.dieccion)
+    console.log('direccion', this.direccion)
     this.venta.direccion = this.direccion
     }else{
       this.msn_error = 'No se obtuvo el codigo de la direccion'
@@ -103,6 +103,7 @@ export default{
             })
           }
           this.venta.total = this.total
+          console.log('total', this.venta.total)
           this.carrito = result.data.cart_general
           
         }); 
@@ -119,7 +120,8 @@ export default{
           }else if(result.data.length >= 1){
            this.msn_error = 'El Id de pago pertenece a otra cuenta'
           }else if(result.data.length == 0){
-             this.crear_compras()
+            /* this.crear_compras()*/
+            console.log(resume)
           }
         })
     },
